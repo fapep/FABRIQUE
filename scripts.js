06d7d22f16ece465b6592eb95ee5b22f1de81f84
fapep <91519812+fapep@users.noreply.github.com>
2021-10-21 20:40:45 +1100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,8 +6,7 @@ function download(content, fileName, contentType) {
     a.click();
 }
 
-function createJSON()
-{
+function createXCP_JSON() {
     // Get values
     const asset = document.getElementById("asset").value
     const issuer_address = document.getElementById("issuer_address").value
@@ -44,4 +43,50 @@ function createJSON()
 
     // Download JSON
     download(jsonLineFormatted, filename, "text/plain")
+}
+
+// Creates address JSON
+window.addresses = []
+function createAddress_JSON() {
+    event.preventDefault();
+    let addressJSON = []
+
+    window.addresses.map((address) => {
+        const value = {"address": address}
+        addressJSON.push(value)
+    })
+    console.log(addressJSON)
+
+    // Download JSON
+    const jsonify = JSON.stringify(addressJSON)
+    const blob = new Blob([jsonify], {type: "application/json"})
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement("a")
+    a.href = url
+    a.download = "Addresses.json"
+    a.click()
+}
+
+// Adds address to list
+function addAddress() {
+    // Get DOM value of text input
+    const address = document.getElementById("addressField").value
+
+    // Append to global addresses
+    window.addresses.push(address)
+
+    // Append DOM to page for user feedback
+    const addressList = document.getElementById("addressList")
+
+    // First reset dom elements
+    addressList.innerHTML = ""
+
+    // Then append list
+    window.addresses.map((address) => {
+        const addressEl = document.createElement("li")
+        addressEl.appendChild(document.createTextNode(address))
+        addressList.appendChild(addressEl)
+    })
+    
+    document.getElementById("addressField").value = ""
 }
